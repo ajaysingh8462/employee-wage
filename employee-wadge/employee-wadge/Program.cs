b61772e61d84992a577603ef97739590afa3f312
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-01 13:12:59 +0530

    [ajay] Employe present or Absent

---
+++ employee-wadge/employee-wadge/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+    int fullTime = 1;
+    Random randobj = new Random();
+    int checkPresent = randobj.Next(0, 2);
+    if(checkPresent==fullTime)
+    {
+        Console.WriteLine("Employee is present");
+    }
+    else
+    {
+        Console.WriteLine("employee is absent");
+    }
+}
