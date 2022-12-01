a4bc9c297dc11798c48e84d59166dc14468a0762
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-01 13:29:13 +0530

    Employe Wage

--- employee-wadge/employee-wadge/Program.cs
+++ employee-wadge/employee-wadge/Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 {
-    int fullTime = 1;
+    int fullTime = 1, Emp_Rate_Per_Hoyr = 20, empHrs=0, empwage = 120;
     Random randobj = new Random();
     int checkPresent = randobj.Next(0, 2);
     if(checkPresent==fullTime)
     {
+        empHrs = 8;
         Console.WriteLine("Employee is present");
     }
     else
     {
+        empHrs = 0;
         Console.WriteLine("employee is absent");
     }
+    empwage = empHrs * Emp_Rate_Per_Hoyr;
+    Console.WriteLine("employee wage:" + empwage);
 }
