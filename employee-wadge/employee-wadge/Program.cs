7f79a32b5edc3bb3c7e605bef13d71fa797f9efa
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-01 14:04:42 +0530

    Employee is part time

--- employee-wadge/employee-wadge/Program.cs
+++ employee-wadge/employee-wadge/Program.cs
@@ -1,18 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 {
-    int fullTime = 1, Emp_Rate_Per_Hoyr = 20, empHrs=0, empwage = 120;
+    int fullTime = 1, partTime = 2, Emp_Rate_Per_Hoyr = 20, empHrs=0, empwage = 0;
     Random randobj = new Random();
-    int checkPresent = randobj.Next(0, 2);
+    int checkPresent = randobj.Next(0, 3);
     if(checkPresent==fullTime)
     {
         empHrs = 8;
         Console.WriteLine("Employee is present");
+            empwage = empHrs * Emp_Rate_Per_Hoyr;
     }
+        else if(checkPresent == partTime)
+        {
+        empHrs = 4;
+        Console.WriteLine("Employee is Part time");
+             empwage = empHrs * Emp_Rate_Per_Hoyr;
+        }
     else
     {
         empHrs = 0;
-        Console.WriteLine("employee is absent");
+        Console.WriteLine("Employee is absent");
     }
-    empwage = empHrs * Emp_Rate_Per_Hoyr;
     Console.WriteLine("employee wage:" + empwage);
 }
