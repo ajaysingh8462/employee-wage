711a269bd2aed1f2fe77bdb71acacab0748a8345
ajaysingh8462 <ajaysingh84625@gmail.com>
2022-12-01 16:25:44 +0530

    20 DAYS WAGE

--- employee-wadge/employee-wadge/Program.cs
+++ employee-wadge/employee-wadge/Program.cs
@@ -5,27 +5,33 @@ using System.Security.Cryptography.X509Certificates;
 {
 int fulTime = 1;
 int partTime = 2;
-int Emp_Rate_Per_Hoyr = 20;    
+int Emp_Rate_Per_Hoyr = 20;
+int Max_Working_Day = 20;
     {
-    int empHrs = 0, empwage = 0;
-    Random randobj = new Random();
-    int checkPresent = randobj.Next(0, 2);
-        switch (checkPresent)
+    int empHrs = 0, empwage = 0, totalErning = 0;
+    for(int day = 0; day < Max_Working_Day; day++)
         {
-            case 1:
-                empHrs = 8;
-                Console.WriteLine("Employee is present");
-                break;
-            case 2:
-                empHrs = 4;
-                Console.WriteLine("Employee is Part time");
-                break;
-            default:
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
-                break;
+            Random randobj = new Random();
+            int checkPresent = randobj.Next(0, 3);
+            switch (checkPresent)
+            {
+                case 1:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is present");
+                    break;
+                case 2:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Part time");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+            empwage = empHrs * Emp_Rate_Per_Hoyr;
+            totalErning += empwage;
+            Console.WriteLine("employee wage:" + empwage);
         }
-    empwage = empHrs * Emp_Rate_Per_Hoyr;
-    Console.WriteLine("employee wage:" + empwage);
+        Console.WriteLine("total employee wage:" + totalErning);
     }
 }
